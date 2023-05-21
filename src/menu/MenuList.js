ce014e4ed2edb268e552dc47f723e5d671b06310
mariered0 <marieakg@gmail.com>
2023-05-21 13:21:33 -0700

    updated to show category names

--- src/menu/MenuList.js
+++ src/menu/MenuList.js
@@ -4,47 +4,27 @@ import NoodleHutApi from "../api/api";
 
 const MenuList = () => {
 
-    // async function getCategoryList(){
-    //     const res = await NoodleHutApi.getCategories();
-    //     console.log('API Call - res:', res);
-    // }
-    // getCategoryList();
-    // async function getUser(){
-    //     const res = await NoodleHutApi.currentUser("test_admin");
-    //     console.log('API Call - res:', res);
-    // }
-    // getUser();
-
-    // const [categories, setCategories] = useState([]);
-    // useEffect(() => {
-    //     getCategoryList();
-    // }, []);
-
-    // async function getCategoryList() {
-    //     const getData = await NoodleHutApi.getCategories();
-    //     setCategories(getData);
-    // }
-    const [items, setItems] = useState([]);
+    const [categories, setCategories] = useState([]);
     useEffect(() => {
         getMenu();
     }, []);
 
     async function getMenu() {
         const getData = await NoodleHutApi.getAllItems();
-        setItems(getData);
+        const categories = Object.keys(getData);
+        setCategories(categories);
     }
-   
 
-    if (!items) return <h1>Loading...</h1>;
+    if (!categories) return <h1>Loading...</h1>;
 
     return (
         <div>
             <h1>Menu list</h1>
 
-                {items.map((item, index, category) => (
+                {categories.map((category, idx) => (
                     <CategoryCard 
-                        key={index}
-                        category={category.categoryName}
+                        key={idx}
+                        category={category}
                     />
                     ))}
 
