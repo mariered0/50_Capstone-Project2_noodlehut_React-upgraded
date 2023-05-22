e982b38619c0b2e39d410912a9f1e34a813b2043
mariered0 <marieakg@gmail.com>
2023-05-21 18:11:11 -0700

    update home page to use material ui

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,13 +1,19 @@
+import { Container, Typography } from "@material-ui/core";
 import React from "react";
 
 function Home() {
-    
-    return (
-        <div>
-                <h1 className="Home-title">NoodleHut</h1>
-        </div>
-    )
-}
+  return (
+    <main>
+      <Container maxWidth="sm">
+        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>NoodleHut
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            Thai home-style coocking. We serve authentically traditional Thai dishes. No dine-in. This is a take-out restaurant.
+        </Typography>
 
+      </Container>
+    </main>
+  );
+}
 
 export default Home;
